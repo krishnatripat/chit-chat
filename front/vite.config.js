84d493ee5de0082d022819fa2317e0aab967be87
krishna tripathi <154440810+krishnatripat@users.noreply.github.com>
2025-10-31 16:51:55 +0530

    Update vite.config.js

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,7 +3,19 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
+
 export default defineConfig({
-     
-  plugins: [react(), tailwindcss()],  
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
+    allowedHosts: ['chit-chat-1-647x.onrender.com']
+  }
 })
+
+
